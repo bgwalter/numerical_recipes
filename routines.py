a9c38254a0f369c529cd24628adf0844591fa572
Brendon Walter <walter@stw.leidenuniv.nl>
2019-03-15 18:31:24 +0100

    add routines for mean, std, pearson coefficient

--- routines.py
+++ routines.py
@@ -2,22 +2,41 @@
 
 # Brendon Walter (s2078864)
 # 2019 March 15
-
 # Numerical Recipes in Astrophysics
 
-import numpy as np  # only for arrays and exp()!
+import numpy as np  # only for arrays
+
+
+def mean(array):
+    '''
+    Calculate the mean of a 1D array
+    '''
+    return sum(array) / len(array)
+
+
+def std(array):
+    '''
+    Calculate the standard deviation of a 1D array
+    '''
+    array = np.asarray(array)
+    numer = sum((array - mean(array))**2)
+    denom = len(array)
+    
+    return (numer / denom)**.5
 
 
 def prod(array):
     '''
-    Calculate the product of a list
+    Calculate the product of an array
     
         prod = x_i * x_{i+1} * ... * x_{N}
+        
+    TODO is there a way to do this without a for loop?
     '''
     total = 1
     for value in array:
-        total *= value
-        
+        total *= value    
+    
     return total
 
 
@@ -27,33 +46,28 @@ def factorial(k):
     
         k! = k * (k-1) * ... * 1
     '''
-    return prod(np.arange(1, k+1))
+    return prod(range(1, k+1))
 
 
-def poisson(l, k):
+def corr_coef(x, y):
     '''
-    Calculate the value k for a Poisson distribution with mean l
+    Pearson correlation coefficient
     
-        P(k) = l^k e^{-l} / k!
+        r_{xy} = (mean(xy) - mean(x)*mean(y)) / (std(x)*std(y))
         
     Parameters
     ----------
-    l : positive float
-        The mean of the distribution
-    k : positive int
-        The number of times an event occurs
+    x, y : list-like
+        Correlation is found between these two lists. Should be the 
+        same length.
+        
+    Returns
+    -------
+    r : float from -1 to 1
+        The correlation between the inputs. -1 or 1 implies perfect
+        correlation, while 0 implies no correlation.
     '''
-    if l < 0:
-        raise ValueError('Negative value passed for lambda')
+    numer = mean(x*y) - mean(x)*mean(y)
+    denom = std(x) * std(y)
     
-    return (l**k * np.exp(-l)) / factorial(k)
-
-
-if __name__ == '__main__':
-    
-    # check prod() and factorial()
-    assert(factorial(5) == 120)
-    
-    # check poisson()
-    assert(round(poisson(1, 0), 3) == 0.368)
-    assert(round(poisson(1, 6), 4) == 0.0005)
+    return numer/denom
