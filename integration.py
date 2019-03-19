bb82eb1167976e544b9f76ffe4fa3d51178fbeba
brendon <vega@lyra>
2019-03-19 12:37:56 +0100

    trapezoid and simpson integration

---
+++ integration.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+
+def trapezoid(func, a, b, N=10000000):
+    '''
+    Integral approximation via the trapezoid rule
+    '''
+    h = (b-a)/N
+    x = np.linspace(a, b, N)
+    
+    y = (func(a) + func(b))/2
+    y += np.sum(func(x))
+    
+    return h*y
+
+
+def simpson(func, a, b, N=10000000):
+    '''
+    Integral approximation via Simpson's rule
+    '''
+    h = (b-a)/N
+    
+    x = np.linspace(a, b, N)
+    y = func(a) + func(b)
+    
+    y += np.sum(2*func(x[0::2]))
+    y += np.sum(4*func(x[1::2]))
+        
+    return (h*y)/3
+
+
+def spherical_integrator(f, a, b):
+    '''
+    Integrate a function over 3 dimensions in spherical coordinates
+    
+        \int \int \int_V f(r) dV
+    
+    where dV = r^2 \sin \phi d\phi d\theta
+    
+    Thus
+        
+        4 \pi \int_a^b = f(r) r^2
+    '''
+    
+    # timing shows that simpson tends to be faster for first integral
+    first = simpson(lambda r: f(r)*r**2, 1e-4, 5)
+    
+    # trapezoid is (slightly) faster for these functions
+    second = trapezoid(np.sin, 0, np.pi)
+    third = trapezoid(lambda theta: 1, 0, 2*np.pi, N=2)
+    
+    return first*second*third
+    
+    
+if __name__=='__main__':
+    
+    # running some tests
+    print('Triple integral to approximate the volume of a sphere (R=5):')
+    print('\tApprox\t', spherical_integrator(lambda r: 1, 0, 5))
+    print('\tReal\t', (4/3) * np.pi * 5**3)
