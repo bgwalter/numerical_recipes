2e38fa92d54c4a6f2996573f1d832b1347c7881d
Brendon Walter <walter@stw.leidenuniv.nl>
2019-04-02 13:50:17 +0200

    add normalize function and formatting for printing

--- gal_profile.py
+++ gal_profile.py
@@ -1,24 +1,47 @@
 #!/usr/bin/env python3
 
+import numpy as np
+
 from RNG import random
+import integration as integ
 
 
 class GalaxyProfile:
     
     def __init__(self, Nsat=100, a=None, b=None, c=None):
-        self.a = a if a is not None else random.randint(1.1, 2.5)
-        self.b = b if b is not None else random.randint(0.5, 2)
-        self.c = c if c is not None else random.randint(1.5, 4)
+        self.a = a if a is not None else random.rand_range(1.1, 2.5)
+        self.b = b if b is not None else random.rand_range(0.5, 2)
+        self.c = c if c is not None else random.rand_range(1.5, 4)
         
         self.Nsat = Nsat
-        self.A = 1  # TODO
+        self.A = self.normalize()
+        
+        
+    def normalize(self, Nsat=None):
+        '''
+        Find the normalization factor A such that 
+        
+            4 \pi \int_0^5 n(x) x^2 = <Nsat>
+        '''
+        if Nsat is None:
+            Nsat = self.Nsat
+            
+        self.A = self.Nsat / integ.spherical_integration(self.dist, 0, 5)
+        return self.A
+    
+    
+    def dist(self, x):
+        return (x/self.b)**(self.a-3) * np.exp(-(x/self.b)**self.c)
         
         
     def n(self, x):
-        norm = self.A * self.Nsat
-        dist = (x/self.b)**(self.a-3) * np.exp(-(x/self.b)**self.c)
-        return norm*dist
+        return self.dist(x) * self.A
     
     
-    def n_log(self, x):
-        pass
+    def __str__(self):
+        s = 'Profile of satellite galaxies:\n' + \
+            'Parameters:\n' + \
+            '\ta = %.4f\tb = %.4f\tc = %.4f\tA = %.4f' \
+            %(self.a, self.b, self.c, self.A)
+        return s
+        
