dff8783d55c292deb8b6b535c343810c2ac26ab8
brendon <vega@lyra>
2019-04-09 12:22:40 +0200

    create tex file for problem 1

--- nur/RNG.py
+++ nur/RNG.py
@@ -5,8 +5,8 @@ Usage: At beginning of program, add the line:
 
     from nur.RNG import random
     
-This will create an instance of the RNG using the current unix time as the 
-starting seed. Generating a random number is then as easy as calling
+This will create an instance of the RNG using the current unix time as 
+the starting seed. Generating a random number is then as easy as calling
 
     random.rand()
 
@@ -16,7 +16,9 @@ To define your own seed, use
     random = RNG(seed)
     random.rand()
 '''
+
 import time
+
 
 class Singleton:
     '''
@@ -26,6 +28,9 @@ class Singleton:
     _shared_state = {}
     def __init__(self):
         self.__dict__ = self._shared_state
+
+
+
 
 class RNG(Singleton):
 
@@ -40,14 +45,12 @@ class RNG(Singleton):
         self.state = int(seed)
         self.max = (2**64)-1   # maximum value (64 bits)
 
-    def __getattr__(self):
-        return getattr(self.instance)
 
-            
     def rand(self):
         '''
-        Generate a random value between 0 and 1 using a combination of a 
-        multiplicative linear congruential generator and a 64-bit XOR-shift.
+        Generate a random value between 0 and 1 using a combination 
+        of a multiplicative linear congruential generator and a 
+        64-bit XOR-shift.
         
         Values for the multipliers taken from Press et al. (2007)
         '''
@@ -74,27 +77,5 @@ class RNG(Singleton):
         return lower + (random.rand() * (upper-lower))
     
 
-
 # random seed from current unix time
 random = RNG(time.time())
-
-    
-if __name__ == '__main__':
-
-    import matplotlib.pyplot as plt
-
-    
-    print('Seed is %s' %random.seed)
-    
-    rand = [random.rand() for _ in range(int(1e6+1))]
-    x, y = rand[:-1], rand[1:]
-
-    plt.figure(figsize=(4,4))
-    plt.scatter(x[:1000], y[:1000], alpha=.5, marker='.')
-    plt.savefig('output/RNG_scatter.png')
-
-    
-    plt.figure(figsize=(5,4))
-    im = plt.hist2d(x, y, bins=20, cmap='Blues')
-    plt.colorbar(im[3])
-    plt.savefig('output/RNG_hist2d.png')
